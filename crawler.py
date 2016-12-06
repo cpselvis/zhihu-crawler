7ab904c4cca41114fd28863254ee97ba22b212db
cpselvis <cpselvis@gmail.com>
2016-12-06 18:03:02 +0800

    Use memory queue and hash set to support BFS.

--- crawler.py
+++ crawler.py
@@ -23,7 +23,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-SEED_URL = "https://www.zhihu.com/people/gaoming623/answers"
+SEED_URL = "https://www.zhihu.com/people/gaoming623"
 
 ## Global variable
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.3"
@@ -49,10 +49,12 @@ class Crawler():
         # Url queue to store crawler task.
         self.url_queue = Queue()
         # Use hash set to store visited urls
-
+        self.visited_url = set()
+        
     def start(self, seed_url):
         # Add seed url to a url queue
         self.url_queue.put(seed_url)
+        self.visited_url.add(seed_url)
         
         # Begin task
         self.task_master()
@@ -67,6 +69,7 @@ class Crawler():
                 break
         
     def send_request(self, url):
+        url = url + "/following"
         # Send a request through requests library and get HTML source
         # file content
         try:
@@ -129,80 +132,86 @@ class Crawler():
         # Iterate followed list and put new url to url task queue.
         for followed_user_item in followed_user_list:
             new_url = "https://www.zhihu.com" + followed_user_item
-            self.url_queue.put(new_url)
+            if new_url not in self.visited_url:
+                self.url_queue.put(new_url)
+                self.visited_url.add(new_url)
 
-    
+    def parse_xpath_source(self, source, index):
+        if source and index < len(source):
+            return source[index]
+        else:
+            return ''
+            
     def get_avatar(self, tree):
         return
         
     def get_username(self, tree):
         username_xpath = "//span[@class='ProfileHeader-name']/text()"
-        return tree.xpath(username_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(username_xpath), 0)
 
     def get_brief_info(self, tree):
         brief_info_xpath = "//span[@class='RichText ProfileHeader-headline']/text()"
-        return tree.xpath(brief_info_xpath)[0]
-
+        return self.parse_xpath_source(tree.xpath(brief_info_xpath), 0)
+        
     def get_industry(self, tree):
         industry_xpath = "//div[@class='ProfileHeader-infoItem'][1]/text()"
-        return tree.xpath(industry_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(industry_xpath), 0)
     
     def get_profession(self, tree):
         profession_xpath = "//div[@class='ProfileHeader-infoItem'][1]/text()"
-        return tree.xpath(profession_xpath)[1]
-
+        return self.parse_xpath_source(tree.xpath(profession_xpath), 1)
 
     def get_education(self, tree):
         education_xpath = "//div[@class='ProfileHeader-infoItem'][2]/text()"
-        return tree.xpath(education_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(education_xpath), 0)
 
     def get_major(self, tree):
         major_xpath = "//div[@class='ProfileHeader-infoItem'][2]/text()"
-        return tree.xpath(major_xpath)[1]
+        return self.parse_xpath_source(tree.xpath(major_xpath), 1)
 
     def get_answer_count(self, tree):
         answer_count_xpath = "//li[@class='Tabs-item' and @aria-controls='Profile-answers']/a/span[@class='Tabs-meta']/text()"
-        return tree.xpath(answer_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(answer_count_xpath), 0)
 
     def get_article_count(self, tree):
         article_count_xpath = "//li[@class='Tabs-item' and @aria-controls='Profile-asks']/a/span[@class='Tabs-meta']/text()"
-        return tree.xpath(article_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(article_count_xpath), 0)
 
     def get_ask_question_count(self, tree):
         ask_question_count_xpath = "//li[@class='Tabs-item' and @aria-controls='Profile-asks']/a/span[@class='Tabs-meta']/text()"
-        return tree.xpath(ask_question_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(ask_question_count_xpath), 0)
 
     def get_collection_count(self, tree):
         collection_count_xpath = "//a[@class='Profile-followStatus'][1]/div/div[@class='Profile-followStatusValue']/text()"
-        return tree.xpath(collection_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(collection_count_xpath), 0)
 
     def get_follower_count(self, tree):
         follower_count_xpath = "//a[@class='Profile-followStatus'][2]/div/div[@class='Profile-followStatusValue']/text()"
-        return tree.xpath(follower_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(follower_count_xpath), 0)
 
     def get_followed_count(self, tree):
         followed_count_xpath = "//a[@class='Profile-followStatus'][1]/div/div[@class='Profile-followStatusValue']/text()"
-        return tree.xpath(followed_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(followed_count_xpath), 0)
 
     def get_follow_live_count(self, tree):
         follow_live_count_xpath = "//a[@class='Profile-lightItem'][1]/span[@class='Profile-lightItemValue']/text()"
-        return tree.xpath(follow_live_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(follow_live_count_xpath), 0)
 
     def get_follow_topic_count(self, tree):
         follow_topic_count_xpath = "//a[@class='Profile-lightItem'][2]/span[@class='Profile-lightItemValue']/text()"
-        return tree.xpath(follow_topic_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(follow_topic_count_xpath), 0)
 
     def get_follow_column_count(self, tree):
         follow_column_count_xpath =  "//a[@class='Profile-lightItem'][3]/span[@class='Profile-lightItemValue']/text()"
-        return tree.xpath(follow_column_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(follow_column_count_xpath), 0)
 
     def get_follow_question_count(self, tree):
         follow_question_count_xpath = "//a[@class='Profile-lightItem'][4]/span[@class='Profile-lightItemValue']/text()"
-        return tree.xpath(follow_question_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(follow_question_count_xpath), 0)
 
     def get_follow_collection_count(self, tree):
         follow_collection_count_xpath = "//a[@class='Profile-lightItem'][5]/span[@class='Profile-lightItemValue']/text()"
-        return tree.xpath(follow_collection_count_xpath)[0]
+        return self.parse_xpath_source(tree.xpath(follow_collection_count_xpath), 0)
         
 # Cases here
 
